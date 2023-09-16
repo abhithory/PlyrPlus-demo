b960fec46096980aa64a3cfcec8bd5359a92184d
abhithory <abhithory@gmail.com>
2023-09-16 01:39:34 -0500

    chapter menu added

--- src/components/PlyrPlus/controls/index.tsx
+++ src/components/PlyrPlus/controls/index.tsx
@@ -2,6 +2,7 @@
 import { VideoChapter } from '@/utils/type/PlyrPlus';
 import Seekbar from './Seekbar';
 import { useState } from 'react';
+import { stringTimeToSeconds } from '../utils/helper';
 
 
 type ControlsProps = {
@@ -14,17 +15,43 @@ type ControlsProps = {
 
 function Controls({ videoRef, chapters, currentTime, setCurrentTime, isControlsVisible }: ControlsProps) {
     const duration = videoRef?.current?.duration || 0;
-
     const [currentChapterIndex, setCurrentChapterIndex] = useState(0);
 
+    const [showChapterMenu, setShowChapterMenu] = useState(false)
 
+
+    const gotoChapter = (chapter: VideoChapter) => {
+        if (!videoRef?.current) return
+        const newTime = stringTimeToSeconds(chapter.timestamp)
+        videoRef.current.currentTime = newTime;
+        setCurrentTime(newTime); // Update the current time in real-time
+        setCurrentChapterIndex(chapter.index);
+        setShowChapterMenu(false)
+    }
     return (
         <>
-            <div className={`plyrPlus__currentchapter_container ${!isControlsVisible && "hidden"}`}>
+            <div className={`plyrPlus__top_container ${!(isControlsVisible || showChapterMenu) && "hidden"}`}>
                 <div className="">
-                    <p>
-                        {">"} {chapters && chapters[currentChapterIndex].chapterName}</p>
+                    <p className='plyrPlus__chapter_btn' onClick={() => {
+                        setShowChapterMenu(prev => !prev)
+                    }}>
+                        {showChapterMenu ? " ^ " : " > "} {" "} {chapters && chapters[currentChapterIndex].chapterName}
+                    </p>
                 </div>
+                {showChapterMenu &&
+                    <div className="plyrPlus__allChapters_menu">
+                        {chapters && chapters.map((i: VideoChapter, key) => {
+                            return (
+                                <div className="plyrPlus__allChapters_menu_item" key={key} onClick={() => {
+                                    gotoChapter(i)
+                                }}>
+                                    <p>{i.timestamp}</p>
+                                    <p>{i.chapterName}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                }
             </div>
             <div className={`video-controls ${!isControlsVisible && "hidden"}`}>
                 <Seekbar duration={duration} currentTime={currentTime} videoRef={videoRef} chapters={chapters} setCurrentTime={setCurrentTime} setCurrentChapterIndex={setCurrentChapterIndex} currentChapterIndex={currentChapterIndex} />
@@ -34,3 +61,4 @@ function Controls({ videoRef, chapters, currentTime, setCurrentTime, isControlsV
 }
 
 export default Controls
+
